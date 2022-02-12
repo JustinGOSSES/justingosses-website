525894ac4ab1e1a94e185558da200799a0acbc92
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2022-02-12 17:52:10 -0600

    changed config to try to deal with images problem in next export that was preventing build

--- next.config.js
+++ next.config.js
@@ -53,6 +53,10 @@ const securityHeaders = [
 ]
 
 module.exports = withBundleAnalyzer({
+  images: {
+    loader: "imgix",
+    path: "https://noop/",
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
